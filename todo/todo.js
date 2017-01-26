d3f17784eb7f515fc5a43f681c101a0a77ee8d3f
BLease <b-lease@kellogg.northwestern.edu>
2017-01-25 22:54:46 -0600

    Commit 1
    1-25-17 After Class

---
+++ todo/todo.js
@@ -0,0 +1,16 @@
+// This code runs when the page loads
+$(function() {
+
+  $(".thumbnail").on("click", function(event) {
+    event.preventDefault()
+    var elementThatWasClicked = $(this)
+    console.log(elementThatWasClicked)
+    elementThatWasClicked.parent().remove()
+  })
+  $(".caption").on("click", function(event) {
+    event.preventDefault()
+    var elementThatWasClicked = $(this)
+    console.log(elementThatWasClicked)
+    elementThatWasClicked.parent().remove()
+  })
+})
